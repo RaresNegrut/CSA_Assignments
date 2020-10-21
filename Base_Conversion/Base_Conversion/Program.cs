4825c6bdfc3263fd8288880fbbe446c1d15fa229
TheJanitor1151 <45803183+TheJanitor1151@users.noreply.github.com>
2020-10-21 18:59:19 +0300

    The program finally works, it only needs a few touches, like a loop that lets me convert an indefinite amount of numbers

--- Base_Conversion/Base_Conversion/Program.cs
+++ Base_Conversion/Base_Conversion/Program.cs
@@ -8,7 +8,7 @@ namespace Base_Conversion
 {
     class Program
     {
-        enum Hextenz
+        enum Multidecimals
         {
             A = 10,
             B,
@@ -20,34 +20,34 @@ namespace Base_Conversion
         //i need a way of storing the power to which the base is raised to, in regards
         //to the position of my element in the string
         //for now, due to time constraints, it will have to wait
-        static void ToDec(ref string shrek, bool IsHex, int NumberBase)
+        static void ToDec(ref string shrek, bool IsBiggerThanDec, int NumberBase)
         {
             double sum = 0;
             int digit, Initial_INT_Index = shrek.Length - 1 - (shrek.Substring(shrek.IndexOf('.'))).Length;
             //Initial_INT_Index is used to compute and store the powers necessary to convert from the original base to decimal
-            if (IsHex)
+            if (IsBiggerThanDec)
                 for (int i = 0; i < shrek.Length; i++)
                 {
                     switch (shrek[i])
                     {
                         case 'A':
-                            digit = (int)Hextenz.A;
+                            digit = (int)Multidecimals.A;
                             break;
 
                         case 'B':
-                            digit = (int)Hextenz.B;
+                            digit = (int)Multidecimals.B;
                             break;
                         case 'C':
-                            digit = (int)Hextenz.C;
+                            digit = (int)Multidecimals.C;
                             break;
                         case 'D':
-                            digit = (int)Hextenz.D;
+                            digit = (int)Multidecimals.D;
                             break;
                         case 'E':
-                            digit = (int)Hextenz.E;
+                            digit = (int)Multidecimals.E;
                             break;
                         case 'F':
-                            digit = (int)Hextenz.F;
+                            digit = (int)Multidecimals.F;
                             break;
                         case '.':
                             digit = 0;
@@ -58,7 +58,6 @@ namespace Base_Conversion
                     }
 
                     sum = sum + (double)digit * (Pow(NumberBase, Initial_INT_Index));
-
 
 
                     if (shrek[i] != '.')
@@ -79,7 +78,7 @@ namespace Base_Conversion
                 }
             shrek = Convert.ToString(sum);
         }
-        static void ToBase(ref string Number, ref string IntegerPart, ref string FractionalPart, int NewBase, bool IsNewHex)
+        static void ToBase(ref string Number, ref string IntegerPart, ref string FractionalPart, int NewBase, bool IsNewBiggerThanDec)
         {
             //this function converts numbers from the decimal numerical system to the desired numerical system
             int index = Number.IndexOf('.');
@@ -97,62 +96,92 @@ namespace Base_Conversion
 
             while (NewIntDigits.Count != 0)
             {
-                IntegerPart += NewIntDigits.Pop().ToString();
+                int top = NewIntDigits.Pop();
+                //if (IsNewBiggerThanDec == true)
+                //{
+                switch (top)
+                {
+                    case 10:
+                        IntegerPart += "A";
+                        break;
+                    case 11:
+                        IntegerPart += "B";
+                        break;
+                    case 12:
+                        IntegerPart += "C";
+                        break;
+                    case 13:
+                        IntegerPart += "D";
+                        break;
+                    case 14:
+                        IntegerPart += "E";
+                        break;
+                    case 15:
+                        IntegerPart += "F";
+                        break;
+                    default:
+                        IntegerPart += top.ToString();
+                        break;
+
+                }
+                //}
+                //else
+                //IntegerPart += top.ToString();
             }
 
 
             int need = FractionalPart.Length;
             int i = 0;
+
             double TempFract = double.Parse("0." + FractionalPart);
+
             Queue<int> NewFracDigits = new Queue<int>();
-            while ((TempFract - (long)TempFract) != 0 || i <= need)
+
+            while ((TempFract - (long)TempFract) != 0 && i <= need)
             {
                 TempFract *= NewBase;
                 NewFracDigits.Enqueue((int)TempFract);
                 TempFract = TempFract - (long)TempFract;
+                i++;
             }
 
             FractionalPart = "";
-            int digit;
 
 
             while (NewFracDigits.Count != 0)
             {
-                if (IsNewHex == true)
+                int head = NewFracDigits.Dequeue();
+                //if (IsNewBiggerThanDec == true)
+                //{
+                switch (head)
                 {
-                    //switch(NewFracDigits.Peek())
-                    //{
-                    //    case 'A':
-                    //        digit = (int)Hextenz.A;
-                    //        break;
-
-                    //    case 'B':
-                    //        digit = (int)Hextenz.B;
-                    //        break;
-                    //    case 'C':
-                    //        digit = (int)Hextenz.C;
-                    //        break;
-                    //    case 'D':
-                    //        digit = (int)Hextenz.D;
-                    //        break;
-                    //    case 'E':
-                    //        digit = (int)Hextenz.E;
-                    //        break;
-                    //    case 'F':
-                    //        digit = (int)Hextenz.F;
-                    //        break;
-                    //    case '.':
-                    //        digit = 0;
-                    //        break;
-                    //    default:
-                    //        digit = NewFracDigits.Dequeue();
-                    //        break;
-                    //}
+                    case 10:
+                        FractionalPart += "A";
+                        break;
+                    case 11:
+                        FractionalPart += "B";
+                        break;
+                    case 12:
+                        FractionalPart += "C";
+                        break;
+                    case 13:
+                        FractionalPart += "D";
+                        break;
+                    case 14:
+                        FractionalPart += "E";
+                        break;
+                    case 15:
+                        FractionalPart += "F";
+                        break;
+                    default:
+                        FractionalPart += head.ToString();
+                        break;
                 }
-                else
-                {
+                //}
+                //else
+                //{
 
-                }
+                //}
             }
 
 
@@ -178,27 +207,26 @@ namespace Base_Conversion
                 Console.WriteLine("We will also need the base you want the number to be converted to");
                 int NewBase = int.Parse(Console.ReadLine());
 
-                bool IsNewHex;
+                bool IsNewBiggerThanDec=false;
 
-                if (NewBase == 16)
-                    IsNewHex = true;
+                if (NewBase > 10)
+                    IsNewBiggerThanDec = true;
                 //in these two strings we will store the integer part and the fractional part of our number
 
                 string IntegerPart = "", FractionalPart = "";
 
-                bool IsHex = false;
+                bool IsBiggerThanDec = false;
                 foreach (char c in Number)
                 {
-                    if ((char.IsLetter(c) && c >= 'A' && c <= 'F') || NumberBase == 16)
-                        IsHex = true;
+                    if ((char.IsLetter(c) && c >= 'A' && c <= 'A' + NumberBase - 10) || NumberBase > 10)
+                        IsBiggerThanDec = true;
                     else
-                        if (((c < 'A' || c > 'F') && char.IsLetter(c)))//TODO: Check for other symbols
+                        if (((c < 'A' || c > 'A' + NumberBase - 10) && char.IsLetter(c)))//TODO: Check for other symbols
                         throw new Exception("Your input contains forbidden characters; please give a valid input");
-                    //if (c == '.' || c == ',')
-                    //StringSeparation(Number, ref IntegerPart, ref FractionalPart, index);
-                    //i think i'll do this bit in the ToBase function
                 }
-                ToDec(ref Number, IsHex, NumberBase);
+                ToDec(ref Number, IsBiggerThanDec, NumberBase);
+                ToBase(ref Number, ref IntegerPart, ref FractionalPart,NewBase, IsNewBiggerThanDec);
+                Number = IntegerPart+ '.' + FractionalPart;
                 Console.WriteLine(Number);
             }
             catch (Exception e)
