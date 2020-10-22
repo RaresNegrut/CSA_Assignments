40cb379363e717a97f893492bf769814713b2de8
TheJanitor1151 <45803183+TheJanitor1151@users.noreply.github.com>
2020-10-22 16:54:23 +0300

    I think it finally works as expected

--- Base_Conversion/Base_Conversion/Program.cs
+++ Base_Conversion/Base_Conversion/Program.cs
@@ -17,18 +17,24 @@ namespace Base_Conversion
             E,
             F
         }
-        //i need a way of storing the power to which the base is raised to, in regards
-        //to the position of my element in the string
-        //for now, due to time constraints, it will have to wait
-        static void ToDec(ref string shrek, bool IsBiggerThanDec, int NumberBase)
+        static bool OnlyInt = false;
+        static void ToDec(ref string Number, bool IsBiggerThanDec, int NumberBase)
         {
+
             double sum = 0;
-            int digit, Initial_INT_Index = shrek.Length - 1 - (shrek.Substring(shrek.IndexOf('.'))).Length;
+            int digit, Initial_INT_Index;
+            if (Number.IndexOf('.') != -1)
+                Initial_INT_Index = Number.Length - 1 - (Number.Substring(Number.IndexOf('.'))).Length;
+            else
+            {
+                Initial_INT_Index = Number.Length - 1;
+                OnlyInt = true;
+            }
             //Initial_INT_Index is used to compute and store the powers necessary to convert from the original base to decimal
             if (IsBiggerThanDec)
-                for (int i = 0; i < shrek.Length; i++)
+                for (int i = 0; i < Number.Length; i++)
                 {
-                    switch (shrek[i])
+                    switch (Number[i])
                     {
                         case 'A':
                             digit = (int)Multidecimals.A;
@@ -53,39 +59,53 @@ namespace Base_Conversion
                             digit = 0;
                             break;
                         default:
-                            digit = shrek[i] - '0';
+                            digit = Number[i] - '0';
                             break;
                     }
 
                     sum = sum + (double)digit * (Pow(NumberBase, Initial_INT_Index));
 
 
-                    if (shrek[i] != '.')
+                    if (Number[i] != '.')
                         Initial_INT_Index--;
                 }
             else
-                for (int i = 0; i < shrek.Length; i++)
+                for (int i = 0; i < Number.Length; i++)
                 {
-                    if (shrek[i] != '.')
-                        digit = shrek[i] - '0';
+                    if (Number[i] != '.')
+                        digit = Number[i] - '0';
                     else
                         digit = 0;
 
                     sum = sum + (double)digit * (Pow(NumberBase, Initial_INT_Index));
 
-                    if (shrek[i] != '.')
+                    if (Number[i] != '.')
                         Initial_INT_Index--;
                 }
-            shrek = Convert.ToString(sum);
+            Number = Convert.ToString(sum);
         }
+        //this function converts from base ten to any base(between binary and hexadecimal)
         static void ToBase(ref string Number, ref string IntegerPart, ref string FractionalPart, int NewBase, bool IsNewBiggerThanDec)
         {
             //this function converts numbers from the decimal numerical system to the desired numerical system
-            int index = Number.IndexOf('.');
-            StringSeparation(Number, ref IntegerPart, ref FractionalPart, index);
-
+            int index;
+            if (Number.IndexOf('.') != -1)
+                index = Number.IndexOf('.');
+            else
+                index = Number.Length - 1;
+            if (OnlyInt == false)
+                StringSeparation(Number, ref IntegerPart, ref FractionalPart, index);
+            else
+                IntegerPart = Number;
             int.TryParse(IntegerPart, out int TempInt);
             IntegerPart = "";
+
+            //if our integer part is 0, we need to concatenate 0 to the string which holds our integer part's digits
+            if (TempInt == 0)
+                IntegerPart += '0';
+
+            //in this stack we'll keep the remainders needed for the integer part of the number
+            //making it easier for us to access them in a specific order
             Stack<int> NewIntDigits = new Stack<int>();
 
             while (TempInt != 0)
@@ -97,8 +117,7 @@ namespace Base_Conversion
             while (NewIntDigits.Count != 0)
             {
                 int top = NewIntDigits.Pop();
-                //if (IsNewBiggerThanDec == true)
-                //{
+
                 switch (top)
                 {
                     case 10:
@@ -124,74 +143,75 @@ namespace Base_Conversion
                         break;
 
                 }
-                //}
-                //else
-                //IntegerPart += top.ToString();
+
             }
 
 
-            int need = FractionalPart.Length;
-            int i = 0;
-
-            double TempFract = double.Parse("0." + FractionalPart);
-
-            Queue<int> NewFracDigits = new Queue<int>();
-
-            while ((TempFract - (long)TempFract) != 0 && i <= need)
+            if (OnlyInt == false)
             {
-                TempFract *= NewBase;
-                NewFracDigits.Enqueue((int)TempFract);
-                TempFract = TempFract - (long)TempFract;
-                i++;
-            }
+                int FracPartLength = FractionalPart.Length;
+                int i = 0;
 
-            FractionalPart = "";
+                double TempFract = double.Parse("0." + FractionalPart);
 
+                Queue<int> NewFracDigits = new Queue<int>();
 
-            while (NewFracDigits.Count != 0)
-            {
-                int head = NewFracDigits.Dequeue();
-                //if (IsNewBiggerThanDec == true)
-                //{
-                switch (head)
+                while ((TempFract - (long)TempFract) != 0 && i <= FracPartLength + 2)//converting from 0.2(10) to binary yields 0.0 as a result; to fix this, i had to let the loop run for some more iterationt
                 {
-                    case 10:
-                        FractionalPart += "A";
-                        break;
-                    case 11:
-                        FractionalPart += "B";
-                        break;
-                    case 12:
-                        FractionalPart += "C";
-                        break;
-                    case 13:
-                        FractionalPart += "D";
-                        break;
-                    case 14:
-                        FractionalPart += "E";
-                        break;
-                    case 15:
-                        FractionalPart += "F";
-                        break;
-                    default:
-                        FractionalPart += head.ToString();
-                        break;
+                    TempFract *= NewBase;
+                    NewFracDigits.Enqueue((int)TempFract);
+                    TempFract = TempFract - (long)TempFract;
+                    i++;
                 }
-                //}
-                //else
-                //{
+                //i suppose a drawback of this program is the inaccuracy caused by having few digits for the fractional part
+                FractionalPart = "";
 
-                //}
+
+                while (NewFracDigits.Count != 0)
+                {
+                    int head = NewFracDigits.Dequeue();
+
+                    switch (head)
+                    {
+                        case 10:
+                            FractionalPart += "A";
+                            break;
+                        case 11:
+                            FractionalPart += "B";
+                            break;
+                        case 12:
+                            FractionalPart += "C";
+                            break;
+                        case 13:
+                            FractionalPart += "D";
+                            break;
+                        case 14:
+                            FractionalPart += "E";
+                            break;
+                        case 15:
+                            FractionalPart += "F";
+                            break;
+                        default:
+                            FractionalPart += head.ToString();
+                            break;
+                    }
+
+                }
             }
-
+            else
+                FractionalPart = "";
 
 
 
         }
+
         static void StringSeparation(string Number, ref string IntegerPart, ref string FractionalPart, int index)
         {
-            FractionalPart = Number.Substring(index + 1);
             IntegerPart = Number.Substring(0, index);
+            if (Number.IndexOf('.') != -1)
+                FractionalPart = Number.Substring(index + 1);
+            else
+                FractionalPart = "";
         }
 
         static void Main(string[] args)
@@ -207,7 +227,7 @@ namespace Base_Conversion
                 Console.WriteLine("We will also need the base you want the number to be converted to");
                 int NewBase = int.Parse(Console.ReadLine());
 
-                bool IsNewBiggerThanDec=false;
+                bool IsNewBiggerThanDec = false;
 
                 if (NewBase > 10)
                     IsNewBiggerThanDec = true;
@@ -224,9 +244,15 @@ namespace Base_Conversion
                         if (((c < 'A' || c > 'A' + NumberBase - 10) && char.IsLetter(c)))//TODO: Check for other symbols
                         throw new Exception("Your input contains forbidden characters; please give a valid input");
                 }
+
                 ToDec(ref Number, IsBiggerThanDec, NumberBase);
-                ToBase(ref Number, ref IntegerPart, ref FractionalPart,NewBase, IsNewBiggerThanDec);
-                Number = IntegerPart+ '.' + FractionalPart;
+                ToBase(ref Number, ref IntegerPart, ref FractionalPart, NewBase, IsNewBiggerThanDec);
+
+                if (OnlyInt == true)
+                    Number = IntegerPart;
+                else
+                    Number = IntegerPart + '.' + FractionalPart;
+
                 Console.WriteLine(Number);
             }
             catch (Exception e)
